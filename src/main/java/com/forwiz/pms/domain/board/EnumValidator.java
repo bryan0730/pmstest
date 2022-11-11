77e6c16da714c801c1ae70cdfdbec877ae093f66
hjlee <hjlee@DESKTOP-M7UCF0M>
2022-11-11 17:54:43 +0900

    페이징, enum valid

---
+++ src/main/java/com/forwiz/pms/domain/board/EnumValidator.java
@@ -0,0 +1,31 @@
+package com.forwiz.pms.domain.board;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+//validator of enum type 
+public class EnumValidator implements ConstraintValidator<ValidEnum, Enum> { // 정의된 어노테이션인 ValidEnum 을 Enum 이용
+	private ValidEnum annotation;
+
+	@Override
+	public void initialize(ValidEnum constraintAnnotation) {
+		this.annotation = constraintAnnotation;
+	}
+
+	// 실제 Validation에 사용할 코드는 isValid 를 통해 구현됨
+//    Enum 의 경우 직접 == 비교가 가능
+	@Override
+	public boolean isValid(Enum value, ConstraintValidatorContext context) {
+		boolean result = false;
+		Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+		if (enumValues != null) {
+			for (Object enumValue : enumValues) {
+				if (value == enumValue) {
+					result = true;
+					break;
+				}
+			}
+		}
+		return result;
+	}
+}
---
+++ src/main/java/com/forwiz/pms/domain/board/dto/BoardSubSelect.java
@@ -0,0 +1,66 @@
+package com.forwiz.pms.domain.board.dto;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Subselect;
+import org.hibernate.annotations.Synchronize;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.forwiz.pms.domain.board.entity.Category;
+import com.querydsl.core.annotations.Immutable;
+
+import lombok.Getter;
+
+@Entity
+@Subselect(
+    "select " +
+    "ROW_NUMBER() over (PARTITION BY b.category ORDER BY b.board_id desc, b.reg_date desc) as Row_Num"
+    + ", b.board_id"
+    + ", b.title"
+    + ", b.category"
+    + ", b.reg_date"
+    + ", b.view_count"
+    + ", p.user_name "
+    + "from board b "
+    + "left outer join pms_user p on b.id = p.id"
+)
+
+@Getter
+@Immutable
+@Synchronize("Board")
+public class BoardSubSelect {
+
+    @Column(name = "Row_Num")
+    private Long rowNum;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "board_id")
+    private Long boardId;
+
+    @Column(name = "title")
+    private String title;
+    
+	@Enumerated(EnumType.STRING)
+    @Column(name = "category")
+    private Category category;
+
+	@CreatedDate
+	@Column(name = "reg_date")
+	private LocalDateTime regDate;
+
+	@Column(name = "view_count")
+    private Long viewCount;
+    
+    @Column(name = "user_name")
+    private String userName;
+    
+}
