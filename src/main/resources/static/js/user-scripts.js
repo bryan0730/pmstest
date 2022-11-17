5988ffcc722337ad827682c8bd2b045c36c90dfa
uihoon <ehdgk9166@naver.com>
2022-11-17 16:33:15 +0900

    사용자 등록 js 유효성검사

--- src/main/resources/static/js/user-scripts.js
+++ src/main/resources/static/js/user-scripts.js
@@ -1,10 +1,36 @@
 
 $(document).ready(function() {
+
+    $("#regist-btn").click(function (){
+        if ($("#id-text").val()==""){
+            alert("아이디는 필수입니다.");
+            return false;
+        }
+        if ($("#pw-text").val()==""){
+            alert("비밀번호는 필수입니다.");
+            return false;
+        }
+        if ($("#rank-text").val()==""){
+            alert("직급은 필수입니다.");
+            return false;
+        }
+        if ($("#name-text").val()==""){
+            alert("이름은 필수입니다.");
+            return false;
+        }
+        if ($("#phone-text").val()==""){
+            alert("휴대폰 번호는 필수입니다.");
+            return false;
+        }
+    });
+
     $("#delBtn").click(function () {
         if (!confirm("삭제하시겠습니까?")) return;
 
         let tdArr = [];
         const checkbox = $("input[name=delYN]:checked");
+
+        console.log("체크박스 리스트 유무 : "+checkbox);
 
         const token = $("meta[name='_csrf']").attr("content");
         const header = $("meta[name='_csrf_header']").attr("content");
