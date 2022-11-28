e6c3096a29146633347af6e97c74d69c468c7802
hjlee <hjlee@DESKTOP-M7UCF0M>
2022-11-28 14:04:32 +0900

    외부 톰캣 설정/ 시작시 ddl none 설정 / 관리자db 한번만 생성되게 수정

--- src/main/java/com/forwiz/pms/Demo1Application.java
+++ src/main/java/com/forwiz/pms/Demo1Application.java
@@ -2,16 +2,22 @@ package com.forwiz.pms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableCaching
 @EnableJpaAuditing
-public class Demo1Application {
-
+public class Demo1Application extends SpringBootServletInitializer{
+	
+	//외부 톰캣 설정 SpringBootServletInitializer 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Demo1Application.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
-
 }
